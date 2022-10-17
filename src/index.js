7bc991842c3cab8961be9e7be7c12944910f92b9
LiekeM <liekevandermade@gmail.com>
2022-10-17 21:38:17 +0300

    Added icon

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function displayWeather(response) {
   humidityElement.innerHTML = "humidity: " + response.data.main.humidity + " %";
   iconElement.setAttribute(
     "src",
-    "http://openweathermap.org/img/wn/10d@2x.png"
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
 
